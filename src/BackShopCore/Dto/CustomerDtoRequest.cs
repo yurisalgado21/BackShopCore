feec88122e6d96626234d1996a38e04c7f2488fe
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-01-12 20:37:44 -0300

    feat: Do not allow empty strings in CustomerDtoRequest properties

--- src/BackShopCore/Dto/CustomerDtoRequest.cs
+++ src/BackShopCore/Dto/CustomerDtoRequest.cs
@@ -5,16 +5,16 @@ namespace BackShopCore.Dto
 {
     public class CustomerDtoRequest
     {
-        [Required(ErrorMessage = ResponseMessages.FirstNameIsRequired)]
+        [Required(ErrorMessage = ResponseMessages.FirstNameIsRequired, AllowEmptyStrings = false)]
         [MaxLength(40, ErrorMessage = ResponseMessages.MaximumCharacters)]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = ResponseMessages.LastNameIsRequired)]
+        [Required(ErrorMessage = ResponseMessages.LastNameIsRequired, AllowEmptyStrings = false)]
         [MaxLength(40, ErrorMessage = ResponseMessages.MaximumCharacters)]
         public string LastName { get; set; }
-        [Required(ErrorMessage = ResponseMessages.EmailIsRequired)]
+        [Required(ErrorMessage = ResponseMessages.EmailIsRequired, AllowEmptyStrings = false)]
         [EmailAddress(ErrorMessage = ResponseMessages.EmailFieldIsNotAValid)]
         public string Email { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
     }
